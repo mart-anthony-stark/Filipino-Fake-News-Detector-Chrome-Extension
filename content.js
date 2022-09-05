c59c1a16d216f3319e35ddf34e16f32a5560713a
mart-anthony-stark <martsalazar0@gmail.com>
2022-09-05 15:54:32 +0800

    fixed content

--- content.js
+++ content.js
@@ -13,7 +13,13 @@ const init = () => {
   // Content div
   const header = document.createElement("div");
   header.className = "ffn-ext-header";
-  header.innerHTML = `<img src='./images/48X48.png' /><h1>${appname}</h1>`;
+  header.innerHTML = `<img src='https://github.com/mart-anthony-stark/Filipino-Fake-News-Detector-Chrome-Extension/blob/master/images/128X128.png?raw=true' />
+                      <div class="ffn-ext-details">
+                          <h1>${appname}</h1>
+                          <span>Made By: Mart Anthony Salazar</span>
+                      </div>
+
+  `;
 
   const content = document.createElement("div");
   content.className = "ffn-ext-content";
@@ -32,13 +38,64 @@ init();
 
 //lead_partner_search_content.js
 chrome.runtime.onMessage.addListener(function (request, sender) {
-  console.log(sender);
-  console.log(request);
-  console.log(request.selection);
-
   const modal = document.querySelector(".ffn-ext-cont");
   const contentDiv = modal.querySelector(".ffn-ext-content");
 
   modal.classList.remove("hidden");
-  contentDiv.innerHTML = `<textarea class="ffn-ext-text" readonly>${request.selection}</textarea>`;
+  contentDiv.innerHTML = `
+  <textarea class="ffn-ext-text" readonly>${request.selection}</textarea>
+  <div class='ffn-ext-spinner hidden'>
+    <div class="lds-ripple"><div></div><div></div></div>
+    <p>Predicting Text...</p> 
+  </div>
+  `;
+
+  predictData(request.selection);
 });
+
+// Predict
+async function predictData(text) {
+  try {
+    const spinner = document.querySelector(".ffn-ext-spinner");
+    spinner.classList.remove("hidden");
+    const contentDiv = document.querySelector(".ffn-ext-content");
+
+    const res = await fetch(
+      "https://app-ed68e554-e20c-4e7a-a33d-e736be04cf3b.cleverapps.io/",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ news: text }),
+      }
+    );
+
+    spinner.classList.add("hidden");
+
+    if (res.status == 200) {
+      const { data, fake_probability, real_probability, prediction_output } =
+        await res.json();
+
+      const resultsDiv = document.createElement("div");
+      resultsDiv.className = "ffn-ext-result";
+      resultsDiv.innerHTML = `
+      <div class="ffn-ext-proba">
+        <h4>Real: ${Math.floor(real_probability)}%</h4>
+        <h4>Fake: ${Math.floor(fake_probability)}%</h4>
+      </div>
+      <div class="ffn-ext-output ${prediction_output}" >
+        ${prediction_output.toUpperCase()} NEWS
+      </div>
+      `;
+
+      contentDiv.appendChild(resultsDiv);
+    }
+  } catch (error) {
+    if (error) {
+      console.log({ error });
+      return { error };
+    }
+  }
+}
